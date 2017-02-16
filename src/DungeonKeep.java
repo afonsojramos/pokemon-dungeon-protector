1345c5abbebf134d54cfe0de64b2f52e9f6469ec
Beatriz Baldaia <bibibaldaia@hotmail.com>
2017-02-16 16:25:17 +0000

    quase pronto

--- src/DungeonKeep.java
+++ src/DungeonKeep.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class DungeonKeep {
 	char board[][] = {{'X','X','X','X','X','X','X','X','X','X'},{'X','H',' ',' ','I',' ','X',' ','G','X'},{'X','X','X',' ','X','X','X',' ',' ','X'},{'X',' ','I',' ','I',' ','X',' ',' ','X'},{'X','X','X',' ','X','X','X',' ',' ','X'},{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},{'X','X','X',' ','X','X','X','X',' ','X'},{'X',' ','I',' ','I',' ','X','k',' ','X'},{'X','X','X','X','X','X','X','X','X','X'}};
+	char guard[] = {'a','s','s','s','s','a','a','a','a','a','a','s','d','d','d','d','d','d','d','w','w','w','w','w'};
 	int x = 1, y = 1; //canto superior esquerdo e a origem do referencial
+	int xg = 8, yg = 1; //posicao do guarda
+	int it = 0; //iterador do array
 	public void update(int new_x, int new_y){
+		if(it == 23){
+			it = 0;
+		}
 		board[y][x] = ' ';
 		x = new_x;
 		y = new_y;
@@ -17,6 +23,21 @@ public class DungeonKeep {
 			board[8][2]= 'S';
 			board[8][4]= 'S';
 		}
+		board[yg][xg]=' ';
+		if(guard[it] == 'a'){
+			xg--;
+		}
+		else if(guard[it] == 'd'){
+			xg++;
+		}
+		else if(guard[it] == 'w'){
+			yg--;
+		}
+		else if(guard[it] == 's'){
+			yg++;
+		}
+		board[yg][xg]='G';
+		it++;
 		
 	}
 	public void display(){
@@ -51,9 +72,10 @@ public class DungeonKeep {
 		d.run();
 	}
 	private void run() {
-		while(x != 0 && (y != 5 || y != 6)){
+		while(x != 0 && (y != 5 || y != 6) && ((y != yg || (y != yg-1) || (y != yg+1)) && (x != xg || x != (xg-1) || x != (xg+1)))){
 			move();
 			display();
 		}
 	}
+	
 }
