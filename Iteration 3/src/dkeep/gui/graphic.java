d9bc3934e175b31545f07cbf95dc82600926e510
Beatriz Baldaia <bibibaldaia@hotmail.com>
2017-03-09 14:50:00 +0000

    a começar a parte gráfica

---
+++ Iteration 3/src/dkeep/gui/graphic.java
@@ -0,0 +1,43 @@
+package dkeep.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class graphic {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					graphic window = new graphic();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public graphic() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
